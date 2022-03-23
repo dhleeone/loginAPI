0d6ec3d73242cb55093ae9c7ddac272d43325637
dhleeone <dhleeone@gmail.com>
2022-03-23 13:27:05 +0900

    Add README.md

--- accounts/constants/message.py
+++ accounts/constants/message.py
@@ -1,14 +1,14 @@
 # success message
 REGISTER_SUCCESS = '회원가입이 완료되었습니다.'
 LOGIN_SUCCESS = '로그인이 완료되었습니다.'
-PASSWORD_CHANE_SUCCESS = '비밀번호 변경이 완료되었습니다.'
+PASSWORD_CHANE_SUCCESS = '패스워드 변경이 완료되었습니다.'
 
 # error message
 CODE_ISSUE_ERROR = '인증번호가 이미 발급되었습니다.'
 REGISTER_VERIFY_ERROR = '전화번호 및 인증번호를 다시 확인해주세요.'
 PW_CHANGE_VERIFY_ERROR = '인증번호를 다시 확인해주세요.'
 CODE_EXPIRED_ERROR = '인증번호가 만료되었습니다.'
-PASSWORD_MISMATCH_ERROR = '비밀번호가 일치하지 않습니다.'
+PASSWORD_MISMATCH_ERROR = '패스워드가 일치하지 않습니다.'
 LOGIN_ERROR = '이메일 혹은 패스워드 오류입니다.'
 ACCESS_ERROR = '접근 권한이 없습니다.'
 EMAIL_NOT_FOUND_ERROR = '등록되지 않은 이메일입니다.'
